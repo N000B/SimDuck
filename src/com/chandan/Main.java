977407e49b6c8fb7d7022873cc588519da5b279a
N000B <inboxofchandan@gmail.com>
2013-10-14 16:44:46 +0530

    This is my first commit for simDuck

---
+++ src/com/chandan/Main.java
@@ -0,0 +1,10 @@
+package com.chandan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("I always waste my fucking time doing this shit");
+        /* write your code here */
+    }
+}
